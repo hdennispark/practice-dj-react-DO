2586648b3009b26151bb714aae2286222579aa5a
Dennis <hdennispark@gmail.com>
2023-06-24 17:35:05 -0400

    made migrations

--- likes/models.py
+++ likes/models.py
@@ -3,11 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
-class Like(models.Model):
-    liked = models.BooleanField()
-
 class LikedItem:
-    like = models.ForeignKey(Like, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
