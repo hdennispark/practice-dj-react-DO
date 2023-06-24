9481cfa4f8b9a2a0f2471d7678324eed9606ca56
Dennis <hdennispark@gmail.com>
2023-06-24 17:04:47 -0400

    generic relations

--- store/models.py
+++ store/models.py
@@ -9,7 +9,7 @@ class Promotion(models.Model):
 
 class Collection(models.Model):
     title = models.CharField(max_length=255)
-    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True)
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
 class Product(models.Model):
     sku = models.CharField(max_length=10, primary_key=True)
