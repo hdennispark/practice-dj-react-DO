d7415c5438cdea85655fd6d82b6ba5187717112b
Dennis <hdennispark@gmail.com>
2023-06-25 00:18:23 -0400

    fixed mysqlclient install error

--- storefront/settings.py
+++ storefront/settings.py
@@ -87,7 +87,10 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'storefront',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'trinitY1@'
     }
 }
 
